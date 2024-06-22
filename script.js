f18153dd2121a91300bcb964f6622f85d5dc554c
coloringwithgray <kayron@houseofcalloway.com>
2024-06-21 19:10:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+window.onload = function() {
+    const text = document.getElementById('mirrorText');
+    text.style.opacity = 1;
+
+    setTimeout(() => {
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+        document.getElementById('content').style.display = 'block';
+    }, 3000);
+};
 
